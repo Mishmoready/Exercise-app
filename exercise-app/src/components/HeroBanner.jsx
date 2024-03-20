cc5d61ff5e16b5d6b7999ca4583092c21796782c
Mishmoready <moses@missionreadyhq.com>
2024-03-20 20:41:48 +1300

    more changes

---
+++ exercise-app/src/components/HeroBanner.jsx
@@ -0,0 +1,49 @@
+import { Box, Typography, Stack, Button } from "@mui/material";
+import HeroBannerImage from "../assets/images/banner.png";
+
+const HeroBanner = () => {
+  return (
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color={"#FF2625"} fontWeight={"600"} fontSize={"26px"}>
+        Fitness Club
+      </Typography>
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "24px", xs: "18px" } }}
+        mb={"23px"}
+        mt={"30px"}
+      >
+        Sweat, Smile <br /> and play with it
+      </Typography>
+      <Typography fontSize={"22px"} lineHeight={"25px"} mb={3}>
+        Get Ready
+      </Typography>
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+      >
+        Search Exercise
+      </Button>
+      <Typography
+        fontWeight={600}
+        color={"#ff2625"}
+        sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+        fontSize="200px"
+      >
+        Exercise
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-image" />
+    </Box>
+  );
+};
+
+export default HeroBanner;
--- exercise-app/src/pages/Home.jsx
+++ exercise-app/src/pages/Home.jsx
@@ -1,10 +1,18 @@
-// import { Link } from "react-router-dom";
-// import { Stack } from "@mui/material";
+// import { useState } from "react";
+import { Box } from "@mui/material";
 
-// import Logo from "../assets/images/Logo.png";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
 
 export default Home;
